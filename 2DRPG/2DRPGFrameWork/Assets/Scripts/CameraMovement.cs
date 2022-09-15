de4ab632b049085039eb3fbaa403f748d3155aa9
shentao <shentao@elex-tech.com>
2022-09-15 21:46:45 +0800

    D: 第四节课教学

--- 2DRPG/2DRPGFrameWork/Assets/Scripts/CameraMovement.cs
+++ 2DRPG/2DRPGFrameWork/Assets/Scripts/CameraMovement.cs
@@ -24,26 +24,14 @@ public class CameraMovement : MonoBehaviour
     void LateUpdate()
     {
         Vector2 posPlayer = new Vector2(player.transform.position.x, player.transform.position.y);
-        Vector2 posCam = new Vector2(this.transform.position.x, this.transform.position.y);
-        
-        if(posPlayer.x >= minBoundingPos.x && posPlayer.y >= minBoundingPos.y && posPlayer.x <= maxBoundingBox.x && posPlayer.y <= maxBoundingBox.y)
-        {
-            reachBounds = false;
-        }
-        else
-        {
-            posPlayer.x = Mathf.Clamp(posPlayer.x, minBoundingPos.x, maxBoundingBox.x);
-            posPlayer.y = Mathf.Clamp(posPlayer.y, minBoundingPos.y, maxBoundingBox.y);
-            this.transform.position = new Vector3(posPlayer.x, posPlayer.y, this.transform.position.z);
-            reachBounds = true;
-        }
 
-        if (posPlayer != posCam && !reachBounds)
-        {
-            Vector3 destination = new Vector3(player.transform.position.x, player.transform.position.y,
-                this.transform.position.z);
-            this.transform.position = Vector3.Lerp(this.transform.position, destination, smoothness);
-        }
+        posPlayer.x = Mathf.Clamp(posPlayer.x, minBoundingPos.x, maxBoundingBox.x);
+        posPlayer.y = Mathf.Clamp(posPlayer.y, minBoundingPos.y, maxBoundingBox.y);
+        Vector3 destination = new Vector3(posPlayer.x, posPlayer.y,
+            this.transform.position.z);
+
+        if (this.transform.position.x == destination.x && this.transform.position.y == destination.y) return;
+        this.transform.position = Vector3.Lerp(this.transform.position, destination, smoothness);
 
     }
 }
