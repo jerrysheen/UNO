60d8a932610c4ce2d351a8c794f859936e97f336
jerrysheen <jerryshin961115@gmail.com>
2022-10-12 00:15:44 +0800

    D： dialogue gfxView

---
+++ Perspective2D/Assets/Scripts/Manager/DialogueSystem/SingleDialogue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SingleDialogue", order = 1)]
+public class SingleDialogue : ScriptableObject
+{
+
+    public Sprite characterASprite;
+    public Sprite characterBSprite;
+
+    public List<Sentence> conversations;
+
+}
+
+[Serializable]
+public class Sentence
+{
+    public string words;
+    public Roles owners;
+}
+
+public enum Roles
+{
+    RoleA, RoleB
+}
---
+++ Perspective2D/Assets/Scripts/Manager/SystemManager.cs
@@ -0,0 +1,27 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// public class SystemManager<T> where T : class
+// {
+//     // Start is called before the first frame update
+//     public void OnCreateSystem()
+//     {
+//         Instance = this;
+//         m_ItemDic = new Dictionary<ItemBase.ItemType, float>();
+//     }
+//
+//     public void OnEnableSystem()
+//     {
+//            
+//     }
+//
+//     public void OnCloseSystem()
+//     {
+//     }
+//
+//     public void OnDestroySystem()
+//     {
+//         Destroy(this);
+//     }
+// }
