f812088780ec64c1d0cbc7846af0e5d58db4d84c
jerrysheen <jerryshin961115@gmail.com>
2022-10-16 01:27:49 +0800

    D: UI system

--- Perspective2D/Assets/Scripts/Manager/DialogueSystem/SingleDialogue.cs
+++ Perspective2D/Assets/Scripts/Manager/DialogueSystem/SingleDialogue.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SingleDialogue", order = 1)]
-public class SingleDialogue : ScriptableObject
+namespace UIManager
 {
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SingleDialogue", order = 1)]
+    public class SingleDialogue : ScriptableObject
+    {
 
-    public Sprite characterASprite;
-    public Sprite characterBSprite;
+        public string npcNameA;
+        public string npcNameB;
+        public Sprite characterASprite;
+        public Sprite characterBSprite;
 
-    public List<Sentence> conversations;
+        public List<Sentence> conversations;
 
-}
+    }
 
-[Serializable]
-public class Sentence
-{
-    public string words;
-    public Roles owners;
-}
+    [Serializable]
+    public class Sentence
+    {
+        public string words;
+        public Roles owners;
+    }
 
-public enum Roles
-{
-    RoleA, RoleB
+    public enum Roles
+    {
+        RoleA, RoleB
+    }
 }
